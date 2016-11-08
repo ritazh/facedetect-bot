c8283ea7cb0c7772bfe489a91472068b67bd3c2c
Rita Zhang <rita.z.zhang@gmail.com>
2016-11-08 00:14:44 -0800

    :framed_picture: Add face detection

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 const restify = require('restify');
 const builder = require('botbuilder');
+const oxford = require('project-oxford'),
+    client = new oxford.Client('40a61586f1ad4dfca462d39e8a4489c1');
 
 //=========================================================
 // Bot Setup
@@ -41,14 +43,52 @@ server.use(restify.bodyParser());
 bot.dialog('/', [
   (session) => {
     session.send('Hello!')
-    builder.Prompts.attachment(session, "Prompts.attachment()\n\nYour bot can wait on the user to upload an image or video. Send me an image and I'll send it back to you.");
+    builder.Prompts.attachment(session, "Upload an image and we will find it.");
   },
   (session, results) => {
-    var msg = new builder.Message(session)
-        .ntext("I got %d attachment.", "I got %d attachments.", results.response.length);
+    // var msg = new builder.Message(session)
+    //     .ntext("I got %d attachment.", "I got %d attachments.", results.response.length);
+    // results.response.forEach(function (attachment) {
+    //     msg.addAttachment(attachment);    
+    // });
+    // session.endDialog(msg);
+    var faces = [];
+    var file;
     results.response.forEach(function (attachment) {
-        msg.addAttachment(attachment);    
+        file = attachment;    
     });
-    session.endDialog(msg);
+    client.face.detect({
+      path: './face.jpeg',
+      returnFaceId: true,
+      analyzesAge: true,
+      analyzesGender: true
+    }).then(function (response) {
+        console.log('The faceid is: ' + response[0].faceId);
+        console.log('The gender is: ' + response[0].faceAttributes.gender);
+        faces.push(response[0].faceId);
+        var msg = "faceid: " +  response[0].faceId + " | gender: " +  response[0].faceAttributes.gender;//new builder.Message(session).ntext("faceid: " +  response[0].faceId + " | gender: " +  response[0].faceAttributes.gender);
+        session.send(msg);
+
+        client.face.detect({
+          path: './face3.jpeg',
+          returnFaceId: true,
+          analyzesAge: true,
+          analyzesGender: true
+        }).then(function (response) {
+            console.log('The faceid is: ' + response[0].faceId);
+            console.log('The gender is: ' + response[0].faceAttributes.gender);
+            faces.push(response[0].faceId);
+            var msg = "faceid: " +  response[0].faceId + " | gender: " +  response[0].faceAttributes.gender;//new builder.Message(session).ntext("faceid: " +  response[0].faceId + " | gender: " +  response[0].faceAttributes.gender);
+            session.send(msg);
+
+            client.face.verify(faces).then(function (response) {
+                console.log(response);
+                console.log(response.isIdentical);
+                console.log(response.confidence);
+                session.endDialog("done");
+              });
+
+        });
+    });
   }
 ]);
