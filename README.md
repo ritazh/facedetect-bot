a335b155f179522d1e6ec3b51f88d11efb0779ba
Rita Zhang <rita.z.zhang@gmail.com>
2016-11-07 23:08:31 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Face Detection Bot using Microsoft Bot Framework
+
+_A bot that detects users in a picture_.
+
+[Check out the demo site](https://facedtectbot.azurewebsites.net/)
+
+![facedetect Bot Demo](botdemo.gif)
+
+## License
+Licensed using the MIT License (MIT); Copyright (c) Microsoft Corporation. For more information, please see [LICENSE](LICENSE).
---
+++ app.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const restify = require('restify');
+const builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3979, function () {
+  console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+console.log('started...')
+console.log(process.env.MICROSOFT_APP_ID);
+var connector = new builder.ChatConnector({
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+server.get('/', restify.serveStatic({
+  'directory': __dirname,
+  'default': 'index.html'
+}));
+//=========================================================
+// Auth Setup
+//=========================================================
+
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(passport.initialize());
+
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', [
+  (session) => {
+    session.send('Hello!')
+    builder.Prompts.attachment(session, "Prompts.attachment()\n\nYour bot can wait on the user to upload an image or video. Send me an image and I'll send it back to you.");
+  },
+  (session, results) => {
+    var msg = new builder.Message(session)
+        .ntext("I got %d attachment.", "I got %d attachments.", results.response.length);
+    results.response.forEach(function (attachment) {
+        msg.addAttachment(attachment);    
+    });
+    session.endDialog(msg);
+  }
+]);
